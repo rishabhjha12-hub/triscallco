9281dc50feaeb4796c73476c954987e7cc22b170
rishabhjha12-hub <76894646+rishabhjha12-hub@users.noreply.github.com>
2023-12-23 23:52:18 +0530

    added home page

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -1,11 +1,5 @@
 import React from "react";
-import {
-  FaFacebook,
-  FaGithub,
-  FaInstagram,
-  FaTwitter,
-  FaTwitch,
-} from "react-icons/fa";
+
 
 const sections = [
   {
@@ -26,13 +20,7 @@ const sections = [
   },
 ];
 
-const items = [
-  { name: "Facebook", icon: FaFacebook, link: "https://www.facebook.com/" },
-  { name: "Instagram", icon: FaInstagram, link: "https://www.instagram.com/" },
-  { name: "Twitter", icon: FaTwitter, link: "https://twitter.com/" },
-  { name: "Twitch", icon: FaTwitch, link: "https://www.twitch.tv/" },
-  { name: "Github", icon: FaGithub, link: "https://github.com/" },
-];
+
 
 const Footer = () => {
   return (
@@ -67,14 +55,14 @@ const Footer = () => {
         </div>
       </div>
 
-      <div className="flex flex-col max-w-[1240px] px-2 py-4 mx-auto justify-between sm:flex-row text-center text-gray-500">
+      {/* <div className="flex flex-col max-w-[1240px] px-2 py-4 mx-auto justify-between sm:flex-row text-center text-gray-500">
         <p className="py-4">Triscall, All rights reserved</p>
         <div className="flex justify-between sm:w-[300px] pt-4 text-2xl">
           {items.map((x, index) => {
             return <x.icon key={index} className="hover:text-white" />;
           })}
         </div>
-      </div>
+      </div> */}
     </div>
   );
 };
